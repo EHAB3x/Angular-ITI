29d2f0549756278d62ae07cc7410d8224ae0d643
EHAB3x <ehabmah6969@gmail.com>
2025-01-04 04:55:08 +0200

    create my 3 components Nav, Home and footer using cmd

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Lecture 1
+# Lecture 1  
+
+## Tips for Angular Component Generation  
+
+To generate a new Angular component, use the following command:  
+
+```bash
+ng g c "Component_Name"
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  // Another Way
+  // template: '<h1>From TS File</h1>',
+  // styles: ['h1{color:green}'],
 })
 export class AppComponent {
   title = 'lec1';
