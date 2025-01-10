ce7834498bf00b06b4637af19f52ca2f54bdd109
EHAB3x <ehabmah6969@gmail.com>
2025-01-10 11:11:37 +0200

    create components and set routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'Home', component: HomeComponent },
+  { path: 'Products', component: ProductsComponent },
+  { path: 'About', component: AboutUsComponent },
+];
