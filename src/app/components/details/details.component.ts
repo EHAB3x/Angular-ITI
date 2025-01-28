423f3d4d092357b94c8db777d4944f226c57221d
EHAB3x <ehabmah6969@gmail.com>
2025-01-28 06:40:36 +0200

    show dynamic details of a product with dynamic routing

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { StaticProductsService } from '../../services/static-products.service';
+import { IProduct } from '../../models/iproduct';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [],
   templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
+  styleUrl: './details.component.css',
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  currentId: number = 0;
+  product : IProduct | null = null;
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private _StaticProductsService: StaticProductsService
+  ) {}
 
+  ngOnInit(): void {
+    this.currentId = Number(this._activatedRoute.snapshot.paramMap.get('id'));
+
+    this.product = this._StaticProductsService.getProductById(this.currentId);
+  }
 }
