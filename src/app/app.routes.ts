7eaa985d68caa73aa052db04ec566082acaed848
EHAB3x <ehabmah6969@gmail.com>
2025-01-28 06:10:10 +0200

    apply nested routing for about us page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,18 @@ import { HomeComponent } from './components/home/home.component';
 import { ProductsComponent } from './components/products/products.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { VisionComponent } from './components/vision/vision.component';
+import { ValuesComponent } from './components/values/values.component';
 
 export const routes: Routes = [
   // First Match Wins
+  { path: '', pathMatch:'full', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'products', component: ProductsComponent },
-  { path: 'about', component: AboutUsComponent },
+  { path: 'about', component: AboutUsComponent, children: [
+    {path:'', pathMatch:'full', redirectTo: 'vision'},
+    {path:'vision', component: VisionComponent},
+    {path:'values', component: ValuesComponent},
+  ] },
   { path: '**', component: NotFoundComponent },
 ];
