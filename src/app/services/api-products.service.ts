93ec18f7dfdda9be87644fca283a430d59b61f62
EHAB3x <ehabmah6969@gmail.com>
2025-03-04 16:55:15 +0200

    create api products service and allow fetching

---
+++ src/app/services/api-products.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiProductsService {
+
+  constructor() { }
+
+  getAllProducts() {}
+
+  getProductById() {}
+
+  getProductsByCatId() {}
+}
