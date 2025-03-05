00e3471a05e99fca3e8cda9fb7b2bc7998dbcf60
EHAB3x <ehabmah6969@gmail.com>
2025-03-05 17:45:31 +0200

    finish add product page

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiCategoriesService } from '../../services/api-categories.service';
+import { ICategory } from '../../models/icategory';
 
 @Component({
   selector: 'app-add-product',
@@ -7,6 +9,19 @@ import { Component } from '@angular/core';
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.css'
 })
-export class AddProductComponent {
+export class AddProductComponent implements OnInit{
+
+  categories !: ICategory[];
+
+  constructor(private _ApiCategoriesService : ApiCategoriesService){}
+
+  ngOnInit(): void {
+    this._ApiCategoriesService.getAllCategories().subscribe({
+      next:(cats)=>{
+        this.categories = cats
+      }
+    });
+  }
+
 
 }
