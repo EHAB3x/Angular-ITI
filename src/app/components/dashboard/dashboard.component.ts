ac19fd5ea3eefcb3b4bc674c2fa6ccd992e5e07f
EHAB3x <ehabmah6969@gmail.com>
2025-03-09 00:22:55 +0200

    add add product button to dashboard page

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiProductsService } from '../../services/api-products.service';
 import { IProduct } from '../../models/iproduct';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
